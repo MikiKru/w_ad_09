a59baeb1707eca155e123b003141b3a219e22033
MikiKru <michal_kruczkowski@o2.pl>
2019-09-09 19:14:29 +0200

    workshop v3

--- workshop_script.py
+++ workshop_script.py
@@ -12,4 +12,13 @@ terms = html.findAll(class_ = "column small-12 large-5")
 editions = html.findAll(class_ = "column small-12 large-3 text-center")
 
 print(terms)
+print(len(terms))
+print(editions)
+print(len(editions))
+
+for index, term in enumerate(terms):
+    terms[index] = str(terms[index]).replace('<div class="column small-12 large-5">', '').replace(' </div>', '')
+    editions[index] = str(editions[index]).replace('<div class="column small-12 large-3 text-center">','')\
+        .replace('</div>','')
+print(terms)
 print(editions)
