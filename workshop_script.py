69e71f30af64163bc67d16ab4ac16ab24226417d
MikiKru <michal_kruczkowski@o2.pl>
2019-09-09 18:53:01 +0200

    workshop v2

--- workshop_script.py
+++ workshop_script.py
@@ -1,5 +1,6 @@
-# ALT + ENTER
-# CTRL + SHIFT + F10
+# ALT + ENTER           - podpowiedź
+# CTRL + SHIFT + F10    - uruchomienie
+# CTRL + D              - duplikowanie lini kodu
 import requests
 import bs4
 
@@ -7,3 +8,8 @@ page = requests.get("https://reaktor.pwn.pl/kurs/analityk-danych/")
 html = bs4.BeautifulSoup(page.content, 'html.parser')
 
 print(html)
+terms = html.findAll(class_ = "column small-12 large-5")
+editions = html.findAll(class_ = "column small-12 large-3 text-center")
+
+print(terms)
+print(editions)
